e1fb7efe68df60e90c19d748a5c396d6689783e0
schoenymjaj <mns@productivityedge.com>
2014-09-16 11:01:54 -0400

    Init Commit

---
+++ SchoenyAzureContactMgr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoenyAzureContactMgr.MvcApplication" Language="C#" %>
---
+++ SchoenyAzureContactMgr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoenyAzureContactMgr.Startup))]
+namespace SchoenyAzureContactMgr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
